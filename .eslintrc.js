46af00e06df89e2dd8e9a3d3678c5c06616e535f
pacexy <pacexy@gmail.com>
2021-02-22 14:36:48 +0800

    chore(eslint): add import/order, react-hooks and other rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,44 @@ module.exports = {
     // https://github.com/prettier/eslint-plugin-prettier
     // https://zhuanlan.zhihu.com/p/80574300
     'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
   ],
+  plugins: ['import'],
+  rules: {
+    'no-console': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc' },
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+  },
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
       extends: ['plugin:@typescript-eslint/recommended'],
       rules: {
+        // note you must disable the base rule as it can report incorrect errors
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         'node/no-missing-import': 'off',
