e59adfd7d580fdb7e2cf946ddd2d5b514a07c18c
pacexy <pacexy@gmail.com>
2021-07-31 21:38:03 +0800

    chore(jest): collect coverage and add ignore pattern

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  testPathIgnorePatterns: ['out'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
